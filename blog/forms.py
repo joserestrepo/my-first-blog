cab64fc6bc6946d5ada2447e10491407c90b81f6
joserestrepo <joserestrepo301@gmail.com>
2018-12-17 15:21:22 -0500

    mejoras en el formulario y termino de crud

--- blog/forms.py
+++ blog/forms.py
@@ -8,3 +8,7 @@ class PostForm(forms.ModelForm):
             'title',
             'text',
         )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['text'].widget.attrs.update({'class': 'form-control'})
