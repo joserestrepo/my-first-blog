1140941cf9c89251d395215aada11e24baf768d5
joserestrepo <joserestrepo301@gmail.com>
2018-12-18 14:27:11 -0500

    conf database heroku

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
